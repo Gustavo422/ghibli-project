b06b8d00fdb2df64a14f8b6d30b7a35ff90a1c1b
Gustavo De Moura <96140566+Gustavo422@users.noreply.github.com>
2025-10-06 18:08:05 -0300

    Add films

--- src/app/ed.jsx
+++
@@ -1,7 +0,0 @@
-export default function Test() {
-  return (
-    <main className="flex-col font-bold h-2xl text-2xl w-4xl flex ">
-      <h1 className="bg-white w-screen text-2xl h-3xl"></h1>
-    </main>
-  );
-}
