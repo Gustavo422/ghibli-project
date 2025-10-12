fa70c6704356f61f7ed0c9540f9dd651e18d5f1d
Gustavo De Moura <96140566+Gustavo422@users.noreply.github.com>
2025-10-11 21:36:54 -0300

    Implement Sign Out and Avatar on Header

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import Header from "@/components/ui/Header";
 import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react";
 import { RocknRoll_One } from "next/font/google";
 
 const rocknroll = RocknRoll_One({
@@ -17,13 +18,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-BR" className={`${rocknroll.variable} h-full`}>
-      <body className="h-screen max-h-screen text-gray-200 antialiased flex flex-col bg-slate-950">
-        <Header />
-        <div className="flex flex-col justify-center items-center mt-[84px] min-h-[calc(100vh-84px)]">
-          {children}
-        </div>
-      </body>
-    </html>
+    <SessionProvider>
+      <html lang="pt-BR" className={`${rocknroll.variable} h-full`}>
+        <body className="h-screen max-h-screen text-gray-200 antialiased flex flex-col bg-slate-950">
+          <Header />
+          <div className="flex flex-col justify-center items-center mt-[84px] min-h-[calc(100vh-84px)]">
+            {children}
+          </div>
+        </body>
+      </html>
+    </SessionProvider>
   );
 }
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,3 +4,29 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+export function placeholderLetters(username) {
+  let letters = [];
+  try {
+    if (username && typeof username === "string" && username.trim()) {
+      username = username.toUpperCase().trim();
+      const arrayUsername = username.split(/\s+/);
+      if (arrayUsername[0]) letters.push(arrayUsername[0][0]);
+      if (arrayUsername[1]) letters.push(arrayUsername[1][0]);
+    }
+    if (letters.length < 2) letters = ["U", "N"]; //Fallback
+  } catch (error) {
+    letters = ["U", "N"];
+    throw new Error(`Failed to create a placeholder - see error:\n${error}`);
+  }
+  return letters.join("");
+}
+
+export function formattedName(username) {
+  const name = username
+    .toLowerCase()
+    .split(" ")
+    .map((letter) => letter.charAt(0).toUpperCase() + letter.slice(1))
+    .join(" ");
+  return name;
+}
